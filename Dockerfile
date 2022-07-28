942304f0e3d227c499656e1967c8e4900e97ae60
L S <myon@foxmail.com>
2022-07-29 07:36:04 +0800

    custom config

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,5 @@ RUN apk add --update --no-cache aria2 && rm -rf /var/cache/apk/*
 COPY entrypoint.sh /
 RUN chmod +x /entrypoint.sh
 
-ENTRYPOINT [ "/entrypoint.sh" ]
+ENTRYPOINT [ "sh","/entrypoint.sh" ]
 CMD aria2c --conf-path=/etc/aria2.conf
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,32 @@ enable-rpc=true
 rpc-listen-all=true
 rpc-secret=${RPC_SECRET:-}
 dir=${DOWNLOAD_DIR:-/downloads}
-log=${DOWNLOAD_DIR:-/downloads}/aria2.log
-max-concurrent-downloads=${CONCURRENT_DOWNLOADS:-4}
-split=${SPLIT:-4}
-max-connection-per-server=${CONNECTIONS_PER_SERVER:-4}
+max-concurrent-downloads=${CONCURRENT_DOWNLOADS:-5}
+piece-length=1M
+min-split-size=${MIN_SPLIT_SIZE:-1M}
+split=${SPLIT:-5}
+max-connection-per-server=${CONNECTIONS_PER_SERVER:-16}
 user-agent=${USER_AGENT:-}
 file-allocation=${FILE_ALLOCATION:-none}
 allow-overwrite=${ALLOW_OVERWRITE:-true}
 auto-file-renaming=${AUTO_FILE_RENAMING:-false}
+disk-cache=${DISK_CACHE:-64M}
+check-integrity=true
+max-tries=10
+retry-wait=10
+max-file-not-found=1
+auto-file-renaming=false
+quiet=${QUIET:-false}
 EOF
 
+fi
+
+if [[ -n "$PUID" && -n "$PGID" ]]; then
+    data_path=/downloads
+    userid=$PUID
+    groupid=$PGID
+    chown -R "$userid":"$groupid" $data_path
+    echo "Running as user $PUID:$PGID"    
 fi
 
 exec "$@"
