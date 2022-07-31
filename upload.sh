e317a4d4f7987822e2153eda98648a6c0d926fa7
L S <myon@foxmail.com>
2022-07-31 19:51:05 +0800

    AUTO_UPDATE

--- upload.sh
+++ upload.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 file_path=$3
 file_name=${file_path##*/}
-rclone move -v $file_path minio:$RCLONE_S3_BUCKET
+uuid=$(cat /proc/sys/kernel/random/uuid)
+now=$(date -Iseconds)
 
-if [ $NOTIFICATION_ENDPOINT ];then
-    curl -i -X POST $NOTIFICATION_ENDPOINT\
-    -H "Content-Type: application/json" \
-    -d '{"type":"object.upload.completed","source":"rclone","data":{"object":"'$file_name'"}}' 
+if [ "$AUTO_UPLOAD" = true ];then
+    rclone move -v $file_path minio:$RCLONE_S3_BUCKET
+    curl -i -X POST http://localhost:3500/v1.0/publish/pubsub/S3ObjectUploaded\
+    -H "Content-Type: application/cloudevents+json" \
+    -d '{"specversion" : "1.0", "type" : "AshLake.Contracts.Collector:S3ObjectUploaded", "source" : "AshLake.Service.Collector", "id" : "'$uuid'", "time" : "'$now'", "datacontenttype" : "application/cloudevents+json", "data":{"objectKey":"'$file_name'"}}' 
 fi
